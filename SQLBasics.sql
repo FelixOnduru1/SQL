f62fef112ef9439c95d16d41fbfe7cf36df26b70
FelixOnduru1 <72564957+FelixOnduru1@users.noreply.github.com>
2020-10-14 23:42:41 +0300

    Start of SQLBasics

---
+++ SQLBasics.sql
@@ -0,0 +1,6 @@
+/*This section looks at the basic application of SELECT and FROM
+SELECT chooses the columns to be displayed on the table.
+FROM chooses the table from which the columns are drawn from.
+A common practice is to use an asterisk to choose all the columns in a particular table */
+SELECT *
+FROM Dimension.City
