f54efb758a70e48e6a2d146e151ae5c8412d5769
FelixOnduru1 <72564957+FelixOnduru1@users.noreply.github.com>
2020-10-15 08:24:59 +0300

    minor changes

--- SQLBasics.sql
+++ SQLBasics.sql
@@ -1,9 +1,9 @@
 /*This section looks at the basic application of SELECT and FROM
 SELECT chooses the columns to be displayed on the table (what you want).
 FROM chooses the table from which the columns are drawn from (where).
-A common practice is to use an asterisk to choose all the columns in a particular table
-LIMIT is used to dispaly a specific number of records from a large database.
-This varies from one server to another in SQL Server the command is SELECT TOP*/
+A common practice is to use an asterisk to choose all the columns in a particular table.
+LIMIT is used to display a specific number of records from a large database.
+This varies from one server to another. In a SQL Server the command is SELECT TOP*/
 
 /* The following code produces the columns City Key, City, and State Province from the table called Dimension.City*/
 SELECT [City Key],
@@ -11,7 +11,7 @@ City,
 [State Province]
 FROM Dimension.City
 
-/*The followiing code selects all columns  fromDimensin.City*/
+/*The followiing code selects all columns  from Dimension.City*/
 SELECT *
 FROM Dimension.City
 
